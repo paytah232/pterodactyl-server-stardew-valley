f705a5c5d21cf6c7aaa7b2b09c5be5b47901999e
Peter Pugno <peterpugno@hotmail.com>
2026-01-06 14:48:52 +0000

    Update VNC Mode

--- server/stardew-valley-server.sh
+++ server/stardew-valley-server.sh
@@ -11,25 +11,25 @@ cleanup() {
 }
 trap cleanup EXIT INT TERM
 
-# Start virtual display if VNC is enabled
-if [ "${USE_VNC}" = "1" ]; then
-    
-    echo "VNC mode enabled: starting Xvfb..."
-    if [ -f /tmp/.X10-lock ]; then rm /tmp/.X10-lock; fi
-    Xvfb :10 -screen 0 1280x720x24 -ac &
+# Start virtual display
+if [ -f /tmp/.X10-lock ]; then rm /tmp/.X10-lock; fi
+Xvfb :10 -screen 0 1280x720x24 -ac &
 
-    # Wait for X display
-    while ! xdpyinfo -display :10 >/dev/null 2>&1; do
-        echo "Waiting for X display..."
-        sleep 2
-    done
+# Wait for X display
+while ! xdpyinfo -display :10 >/dev/null 2>&1; do
+    echo "Waiting for X display..."
+    sleep 2
+done
+
+# Start VNC if enabled
+if [ "${USE_VNC}" = "1" ]; then
 
     echo "Starting x11vnc on port ${VNC_PORT}"
 
     # Start VNC server
     x11vnc -display :10 -rfbport "${VNC_PORT:-5900}" -ncache 10 -forever -shared -passwd "${VNC_PASS:-myvncpassword}" &
 else
-    echo "VNC mode disabled: skipping Xvfb/i3/x11vnc."
+    echo "VNC mode disabled"
 fi
 
 # Start Stardew server
